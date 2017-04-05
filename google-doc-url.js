15fb9c1039e8206bbee3f89e0544905775dd91c4
bruns87 <brunogaray87@gmail.com>
2017-04-05 11:03:31 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hwxDUPIk8lcCYtfcE_jFV569D1ZQ49gqkJOkP6X2ZpY/edit#gid=1716207954';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hwxDUPIk8lcCYtfcE_jFV569D1ZQ49gqkJOkP6X2ZpY/edit#gid=0';
